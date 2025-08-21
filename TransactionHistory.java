7af04541d85ee383ba2638f698cf6e2dbe1ac5a8
fatima <fatima.amani65@gmail.com>
2025-08-21 09:36:17 +0530

    add output images

--- TransactionHistory.java
+++ TransactionHistory.java
@@ -11,7 +11,7 @@ public final class TransactionHistory {
     private final double transactionAmount;
     private final double remainingBalance;
 
-    public TransactionHistory(String transactionType, double transactionAmount, double remainingBalance) {
+    TransactionHistory(String transactionType, double transactionAmount, double remainingBalance) {
         this.transactionId = UUID.randomUUID().toString();
         this.transactionDate = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now());
         this.transactionTime = Timestamp.valueOf(LocalDateTime.now()).toString();
@@ -22,12 +22,12 @@ public final class TransactionHistory {
 
     public void displayTransactionHistory() {
         System.out.println("Transaction Details: ");
-        System.out.println("Transaction Id: " + transactionId);
-        System.out.println("Transaction Date: " + transactionDate);
-        System.out.println("Transaction Time: " + transactionTime);
-        System.out.println("Transaction Type: " + transactionType);
-        System.out.println("Transaction Amount: " + transactionAmount);
-        System.out.println("Remaining Balance: " + remainingBalance);
+        System.out.print(" Transaction Id: " + transactionId);
+        System.out.print(" Transaction Date: " + transactionDate);
+        System.out.println(" Transaction Time: " + transactionTime);
+        System.out.print(" Transaction Type: " + transactionType);
+        System.out.print(" Transaction Amount: " + transactionAmount);
+        System.out.print(" Remaining Balance: " + remainingBalance);
     }
 
 }
